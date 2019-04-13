f354b08a8bfbb1c2862158b2435104d3880cc9fa
KeckeAna <akeckes@ekobit.hr>
2019-04-13 10:40:03 +0200

    add translations on grid data

--- src/app/flower-type/flower-type.component.ts
+++ src/app/flower-type/flower-type.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { FlowerType } from '../shared/flower-type';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 
 @Component({
   selector: 'flower-type-component',
@@ -11,12 +12,24 @@ export class FlowerTypeComponent implements OnInit {
 
   items: Array<FlowerType>;
 
+  constructor(private translateService: TranslateService) { }
+
   ngOnInit(): void {
-    this.items =  [
-      {id: 1, type: 'Bouquet'},
-      {id: 2, type: 'Jar'},
-      {id: 3, type: 'Seedling'}
-    ];
+    this.translateService.get('FLOWER_TYPE.TYPES').subscribe(res => {
+      this.items = [
+        { id: 1, type: res.BOUQUET },
+        { id: 2, type: res.JAR },
+        { id: 3, type: res.SEEDLING }
+      ];
+    });
+
+    this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.items = [
+        { id: 1, type: event.translations.FLOWER_TYPE.TYPES.BOUQUET },
+        { id: 2, type: event.translations.FLOWER_TYPE.TYPES.JAR },
+        { id: 3, type: event.translations.FLOWER_TYPE.TYPES.SEEDLING }
+      ];
+    });
   }
 
 }
